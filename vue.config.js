3c45e27f73aa07dd5d4350217eecaf49de6b5af8
hyeminnoh <shrmffkfk@naver.com>
2020-04-15 04:36:56 +0900

    [Update] vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 // vue.config.js
 module.exports = {
-    publicPath: '/hyem-devfolio/'
-}
+    publicPath: '/hyem-devfolio/',
+    chainWebpack: config => {
+        config.module
+          .rule('images')
+            .use('url-loader')
+              .loader('url-loader')
+              .tap(options => Object.assign(options, { limit: 10240 }))
+      }
+    }
