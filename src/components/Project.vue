2ae23ed3e76ade46ae91911d4398ea42db3f1c99
hyeminnoh <shrmffkfk@naver.com>
2020-04-15 23:58:49 +0900

    [Add] Project List component

---
+++ src/components/Project.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section">
+    <p>Project</p>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Project',
+  components: {
+  }
+}
+</script>
+<style>
+body {
+  font-family: arial, helvetica;
+}
+.section {
+  position: relative;
+  text-align: center;
+}
+</style>
